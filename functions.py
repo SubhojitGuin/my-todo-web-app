92dc536720583e97f63ac5168aa96df29c9794a2
SubhojitGuin <subhojitguin2004@gmail.com>
2023-06-11 01:02:48 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """Read the text file and return a list of to-do items"""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_local, filepath=FILEPATH):
+    """Write the to-do items list in the text file"""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_local)
+
+
+if __name__ == "__main__":
+    print(get_todos())
